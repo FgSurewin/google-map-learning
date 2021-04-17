3d160b9d084866ad756b2b263ef5071275756bea
FgSurewin <mrliu3244794@gmail.com>
2021-04-16 23:42:08 -0400

    Carousel  -V2

--- src/components/Carousel/data.js
+++ src/components/Carousel/data.js
@@ -1,1 +1,23 @@
-export const CarouselData = {};
+import Curve from "../../images/curve.svg";
+import Girl from "../../images/girl.png";
+
+export const CarouselData = {
+	curve: Curve,
+	slides: [
+		{
+			icon: Girl,
+			name: "mary",
+			content: "First",
+		},
+		{
+			icon: Girl,
+			name: "mary",
+			content: "Second",
+		},
+		{
+			icon: Girl,
+			name: "mary",
+			content: "Third",
+		},
+	],
+};
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,29 +1,46 @@
 import React from "react";
-// import Swiper core and required modules
-import SwiperCore, { Navigation } from "swiper";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
+// import Swiper JS
+import Swiper, { Navigation } from "swiper";
+// import Swiper styles
+import "swiper/swiper-bundle.css";
 import "./style.scss";
+import { CarouselData } from "./data";
 
-// install Swiper modules
-SwiperCore.use([Navigation]);
+Swiper.use([Navigation]);
+
 export default function Carousel() {
+	React.useEffect(() => {
+		const swiper = new Swiper(".swiper-container", {
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+		console.log(swiper);
+	}, []);
+
 	return (
-		<Swiper
-			spaceBetween={100}
-			slidesPerView={1}
-			navigation
-			onSwiper={(swiper) => console.log(swiper)}
-			onSlideChange={() => console.log("slide change")}
-		>
-			<SwiperSlide>Slide 1</SwiperSlide>
-			<SwiperSlide>Slide 2</SwiperSlide>
-			<SwiperSlide>Slide 3</SwiperSlide>
-			<SwiperSlide>Slide 4</SwiperSlide>
-		</Swiper>
+		<div className="Carousel">
+			<img src={CarouselData.curve} alt="curve" />
+			<div className="swiper-cover">
+				<div className="swiper-container">
+					<div className="swiper-wrapper">
+						{CarouselData.slides.map((slide, index) => (
+							<div className="swiper-slide" key={index}>
+								<div className="slide-container">
+									<img src={slide.icon} alt="icon" className="slide-icon" />
+									<div>
+										<p className="slide-name">{slide.name}</p>
+										<p className="slide-content">{slide.content}</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className="swiper-button-next"></div>
+					<div className="swiper-button-prev"></div>
+				</div>
+			</div>
+		</div>
 	);
 }
